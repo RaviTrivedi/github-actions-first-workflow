8bf203db43d86694fe70e2da804fecc06d90fafa
RaviTrivedi <trivediravi55@gmail.com>
2025-11-11 08:48:24 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        commit 1
+      </h1>
+      <h2>
+        commit 2
+      </h2>
+    </>
+  )
+}
+
+export default App
